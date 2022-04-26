1d355a7c591b79c80987b12c756c7113ac686dff
Giuseppe <giuseppefvr@gmail.com>
2022-04-26 22:07:11 +0200

    feat: added select tag in cities component

--- src/components/cities.jsx
+++ src/components/cities.jsx
@@ -1,8 +1,22 @@
+const data = ["Milano", "Bologna", "Palermo", "Genova", "Torino"];
+
+
 const Cities = () => {
 
-    return (
-        <section className="card__cities">
+    const cityChange = (event) => {
+        const selected = event.target.value;
+        console.log(selected);
+        
+    }
 
+    return (
+
+        <section className="card__cities">
+            <select onChange={cityChange}>
+                {data.map((item, index) => (
+                    <option key={index} value={item.toLocaleLowerCase()}>{item}</option>
+                ))}
+            </select>
         </section>
 
     );
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,6 +1,7 @@
 const Intro = () => (
     <section className="card__intro">
-
+        <h1>Weather widget</h1>
+        <p>Questo widget mostra il meteo per la città selezionata</p>
     </section>
 );
 
